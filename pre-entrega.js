b128ff9fd9e1186abaf71d21901bbb790be13d38
RafDes21 <israeltineo84@gmail.com>
2022-03-04 15:58:23 -0300

    cambio random

--- pre-entrega.js
+++ pre-entrega.js
@@ -54,12 +54,12 @@ switch (opcionCasos) {
         negativos.forEach(element => {
             const numeroRandom = Math.round(Math.random() * 4)
 
-            alert(` Sr. ${element}\n resultado: Negativo.\n Acerque a ${direccion[numeroRandom]} para vacunarse entre las 08:00 hrs a 13:00 hrs\n`)
+            alert(` Sr. ${element}\n resultado: Negativo.\n Acerque a ${direccion[numeroRandom]}  para vacunarse entre las 08:00 hrs a 13:00 hrs\n`)
 
         });
         positivos.forEach(element => {
             const numeroRandom = Math.round(Math.random() * 4)
-            alert(` Sr. ${element}\n resultado: Positvo.\n Debe dirigirse a ${hoteles[numeroRandom]} donde estará aislado por 14 días\n`)
+            alert(` Sr. ${element}\n resultado: Positvo.\n Debe dirigirse a ${hoteles[numeroRandom]}  donde estará aislado por 14 días\n`)
 
         });
 
