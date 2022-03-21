ba88e840895a65ccdd9348167ea582f650823158
RafDes21 <israeltineo84@gmail.com>
2022-03-20 21:34:24 -0300

    cambios

--- pre-entrega.js
+++ pre-entrega.js
@@ -1,80 +1,40 @@
-alert("         ****Ministerio de Salud****\n\n\n---> Prueba de covid <---")
-
-const datos = []
-const sintomas = ["fiebre", "tos", "cansancio", "perdiDelOlfato", "dolor de cabeza", "dolor de garganta"]
-const positivos = []
-const negativos = []
-const direccion = ["Regimiento Patricios 1941", "Humberto 1º 470", "Almirante Brown 240", "	Montes de Oca 40", "Prefectura Naval Argentina 80"]
-const hoteles = ["Hotel 13 de junio", "Hotel 13 de junio", "Recoleta Luxury Boutique Hotel ", "Exe Hotel ColónSe", "Palacio Paz Hotel"]
-let cupos
-let principalesSintomas
-
-do {
-    cupos = parseInt(prompt("Ingrese la cantidad de pacientes."))
-
-} while (isNaN(cupos) || cupos === 0);
-
-do {
-
-    let nombre = prompt("Ingrese los Nombres y Apellidos");
-    while (nombre == "") {
-
-        alert("debe ingresar un nombre")
-        nombre = prompt("Ingrese los Nombres y Apellidos");
-    }
-
-    datos.push(nombre);
 
 
-    do {
+const boton = document.getElementById(`boton`)
+const datosNuevos = []
 
-        principalesSintomas = parseInt(prompt("    -----SINTOMAS-----\n fiebre.\n Tos.\n cansancio.\n Perdida de Olfato.\n Dolor de cabeza.\n Dolor de Garganta \n\n¿Ingrese la CANTIDAD de síntomas"))
-
-    } while (isNaN(principalesSintomas) || principalesSintomas === 0);
-
-    function cantidadSintomas(n1) {
-        return sintomas.length - n1
-    }
+boton.addEventListener(`click`, () => {
+    const nombre = document.getElementById(`nombre`).value
+    const apellido = document.getElementById(`apellido`).value
+    const dni = document.getElementById(`dni`).value
 
 
-    if (cantidadSintomas(principalesSintomas) < 3) {
-        positivos.push(nombre)
 
-    } else {
-        negativos.push(nombre)
+    const datosPersonas = {
+        nombre: nombre,
+        apellido: apellido,
+        dni: dni
 
     }
+    
+    datosNuevos.push(datosPersonas)
 
-} while (datos.length != cupos)
+    localStorage.setItem(`datos`, JSON.stringify(datosNuevos))
+   formatear.reset()
+})
 
-alert("Datos guardados...")
 
 
-const opcionCasos = parseInt(prompt("elija una opción \n1° Resultado y derivaciones.\n2°Reportar un error y empezar de nuevo..."))
+result.addEventListener(`click`, () => {
 
-switch (opcionCasos) {
-    case 1:
-        alert("     Resultados y Derivaciones")
-        negativos.forEach(element => {
-            const numeroRandom = Math.round(Math.random() * 4)
+    const listaNueva = JSON.parse(localStorage.getItem(`datos`))
 
-            alert(` Sr(a). ${element.toUpperCase()}\n resultado: Negativo.\n Acerque a ${direccion[numeroRandom]}  para vacunarse entre las 08:00 hrs a 13:00 hrs.\n`)
+    for (const i of listaNueva) {
+        const lista = document.getElementById(`lista`)
+        const listaPersonas = document.createElement(`li`)
+        listaPersonas.innerText = `NOMBRE: ${i.nombre} ----- APELLIDO: ${i.apellido} ----DNI: ${i.dni}`
 
-        });
-        positivos.forEach(element => {
-            const numeroRandom = Math.round(Math.random() * 4)
-            alert(` Sr(a). ${element.toUpperCase()}\n resultado: Positivo.\n Debe dirigirse a ${hoteles[numeroRandom]}  donde estará aislado por 14 días...\n`)
+        lista.appendChild(listaPersonas)
+    }
 
-        });
-
-        break;
-    case 2:
-
-        alert("¡ERROR REPORTADO! \n\n Usted empezará de nuevo a cargar los datos...")
-        location.reload()
-
-        break;
-
-    default:
-        break;
-}
+})
